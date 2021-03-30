ad2aa942e4522be83eccece44d0a0ff2356a50d5
zhufz <zhufz3528ui@126.com>
2021-03-30 22:03:02 +0800

    first commit

---
+++ src/test/java/db2/elibrary/ElibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package db2.elibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
