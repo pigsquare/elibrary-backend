7dc546aea686592f39c61e499596612b3ec43a0c
zhufz <zhufz3528ui@126.com>
2021-04-03 23:08:13 +0800

    feat: use alipay

--- src/main/java/db2/elibrary/controller/TestRestController.java
+++ src/main/java/db2/elibrary/controller/TestRestController.java
@@ -1,10 +1,13 @@
 package db2.elibrary.controller;
 
+import com.alipay.api.AlipayApiException;
 import db2.elibrary.dto.AddUserTestDto;
 import db2.elibrary.entity.User;
 import db2.elibrary.service.AuthService;
 import db2.elibrary.service.MailService;
 import db2.elibrary.service.SmsService;
+import db2.elibrary.util.AliPayUtil;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.*;
@@ -30,6 +33,17 @@ public class TestRestController{
     public String testSms(){
         // mailService.sendTextMail("zhufz3528ui@126.com","测试邮件","图书管理系统-进行测试\n普通邮件测试~",mailSender);
         return smsService.sendReservationSuccessSms("","《计算机体系结构》","3月28日",7,"4月10日");
+    }
+    @RequestMapping("/alipay/{barcode}")
+    public String testAlipay(@PathVariable String barcode) throws AlipayApiException {
+        AliPayUtil aliPayUtil = new AliPayUtil();
+        Boolean res = true;
+        try {
+            res = aliPayUtil.payLateFee(1.39,barcode);
+        }catch (AlipayApiException e){
+            e.printStackTrace();
+        }
+        return res.toString();
     }
 
 }
--- src/main/java/db2/elibrary/util/AliPayUtil.java
+++ src/main/java/db2/elibrary/util/AliPayUtil.java
@@ -1,287 +1,42 @@
 package db2.elibrary.util;
 
+import com.alipay.api.AlipayApiException;
 import com.alipay.api.AlipayClient;
-import com.alipay.api.AlipayResponse;
 import com.alipay.api.DefaultAlipayClient;
-import com.alipay.api.domain.TradeFundBill;
-import com.alipay.api.internal.util.StringUtils;
-import com.alipay.api.response.AlipayTradeQueryResponse;
-import com.alipay.demo.trade.config.Configs;
-import com.alipay.demo.trade.model.ExtendParams;
-import com.alipay.demo.trade.model.GoodsDetail;
-import com.alipay.demo.trade.model.builder.AlipayTradePayRequestBuilder;
-import com.alipay.demo.trade.model.builder.AlipayTradeQueryRequestBuilder;
-import com.alipay.demo.trade.model.builder.AlipayTradeRefundRequestBuilder;
-import com.alipay.demo.trade.model.result.AlipayF2FPayResult;
-import com.alipay.demo.trade.model.result.AlipayF2FQueryResult;
-import com.alipay.demo.trade.model.result.AlipayF2FRefundResult;
-import com.alipay.demo.trade.service.AlipayMonitorService;
-import com.alipay.demo.trade.service.AlipayTradeService;
-import com.alipay.demo.trade.service.impl.AlipayMonitorServiceImpl;
-import com.alipay.demo.trade.service.impl.AlipayTradeServiceImpl;
-import com.alipay.demo.trade.service.impl.AlipayTradeWithHBServiceImpl;
-import com.alipay.demo.trade.utils.Utils;
-import com.alipay.easysdk.kernel.Config;
-import com.alipay.easysdk.payment.facetoface.*;
-import com.alipay.api.domain.AlipayTradeServiceSignModel;
+import com.alipay.api.domain.AlipayTradePayModel;
+import com.alipay.api.request.AlipayTradePayRequest;
+import com.alipay.api.response.AlipayTradePayResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.jdbc.Null;
-import org.checkerframework.checker.units.qual.A;
-
-import java.io.ObjectInputFilter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 @Slf4j
-public class AliPayUtil {
-    // 支付宝当面付2.0服务
-    private static AlipayTradeService   tradeService;
+public class AliPayUtil{
+    public Boolean payLateFee(Double fee, String barCode) throws AlipayApiException {
+        AlipayClient alipayClient = new DefaultAlipayClient(
+                "https://openapi.alipaydev.com/gateway.do",
+                "2021000119644643",
+                "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCmp36sDNSrjsSyDiLuVRFz37JRdBKdWa/dbmWpepZ2Jp4A+mehVSBt9Y80lOWk9CRvbBMqoQNAFzYn07RoUZQgJGSi6c8k2dHzjDI3aMiQ591C01Iv4GmQWZqLfTwDhh39caTQR6G3PUSVhF9gZyINlvjYy0eakNfJOGyjCKYtu3CcG0zxuEpf5T4r0pjNz0v4JCivIBTHsd2/Y9DeL8tLp2jAT470ITvwu8/A/fCJ2IlufBTE8LLAkAqdUdC4rPRhbNuh/OHqI2whoyG1zOX8z6t+PF79wFTgOuZKhT7AFNupHej9ZS8WoV0hQ3epvU4B+mRzdDBHImkNX3wDDCUnAgMBAAECggEBAIQB2MzfvPXHuPZxTCoNKC3o2465qqUJDc0QTWbZu6ULIc0UjFG25GE1SaIU73KiENpReT0jHTf/Kt6RSy32sKZlLXH+Uh6Wiz+npX9OeNCPWSw9x7Q0xmmZEFm9cXQ5IDhJdIumseo7lb/5grMR/l9VXvgazTUZ1lOFZkgd6cP+ldHg6OmJlGgMg7gSfpD8w7L6XhMRKgp67Jb3BFeJuuIRbuJo+0bdhpcFqFpG+gFniRvysTY/zNSrGGu7KfdOThbWdaYgRdPTF+nQM6/InOl+9dvZv8/Bk052a6KWvI/WO1Ub0k7U1EIBfks1TLsktvEWct8H12/GscRzuSzsCAECgYEA1tyLiXXFvc3Zdms4CfGODMWz8E+FuXGsNXgZTf1sqD7p/aqe9SNZ3Lk1oJAVusVSy9kkQywXFKaWOUmtOD0y80gbBvRCdHVc06ZX3B5SqP0UZd470y+uyz3F9fUb0cDxATXA7XxHB05U66hJMvJB98JybvidS2VtAnt6V7mjgqcCgYEAxpARgWQbReewv1Obv5DOskco7Sb+dY2fwk6ceZMXjQlxtNcAlIehyY4tikjbfE4K1k1db1GsvwV2JLF66Cl0sPKihrM446HdOOf7ZScjYfRZtj6tcYXdmsGdZ9OvGeEuxAaZlSeHKOSOj/H/KSNVsKRhtgPXN5z0PTWXWvaTGYECgYB/EuDT43uXze8XQkJaXyqME0FglyeXjcsztFwC7CcfOlZZWFe607WJ/V/iHlU7qQhW143CzIi6BexC5Xg1ErzXdYXGjx/ekKrd4juES8rHdyZhORZYyJKwUVCLLvPNEjd3Z7s6uY2onINM/LKOFwk85xuW1iLxPINEPJMW3wv9cQKBgAzIW4gj5KmUZ8l5XW5wqQ3LDPRvbTQuINnGa8Zb1MlMkVhd+viU3rJ2B7J+TDrtXz86jchLzr4hYOCt2Ea33H5DsHjRctkco3w1WvbBFUMKEvhrgojrYD/FNRfpBb25xGP0NNDoUYgtZowXvSBt2OddPbkU+DbpUMPb/llpxECBAoGAQ1yQ+wowqphcp5rb2TU54aMHc5S817+WG8oWYiaYrT4uQIAdskVd/I1IZoedcB09IXgy3rT3TW3JgJHJbQjBhSL0dnb3s/Zqh80p1xT1or/ikL/y6UNe62LwtreqPIXx6lWKBCt4I/kydJOM7zGLrxSakTBDgh0eKDSM6IFSTm8=",
+                "json","utf-8",
+                "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDIgHnOn7LLILlKETd6BFRJ0GqgS2Y3mn1wMQmyh9zEyWlz5p1zrahRahbXAfCfSqshSNfqOmAQzSHRVjCqjsAw1jyqrXaPdKBmr90DIpIxmIyKXv4GGAkPyJ/6FTFY99uhpiq0qadD/uSzQsefWo0aTvP/65zi3eof7TcZ32oWpwIDAQAB",
+                "RSA"
+                );
+        AlipayTradePayRequest request = new AlipayTradePayRequest();
+        AlipayTradePayModel model = new AlipayTradePayModel();
+        request.setBizModel(model);
 
-    // 支付宝当面付2.0服务（集成了交易保障接口逻辑）
-    private static AlipayTradeService   tradeWithHBService;
+        model.setOutTradeNo(System.currentTimeMillis()+"");
+        model.setSubject("缴纳滞纳金"+fee+"元");
+        model.setTotalAmount(fee.toString());
+        model.setAuthCode(barCode);//沙箱钱包中的付款码
+        model.setScene("bar_code");
 
-    // 支付宝交易保障接口服务，供测试接口api使用，请先阅读readme.txt
-    private static AlipayMonitorService monitorService;
-    static {
-        Configs.init("classpath:zfbinfo.properties");
-
-        tradeService = new AlipayTradeServiceImpl.ClientBuilder().build();
-
-        // 支付宝当面付2.0服务（集成了交易保障接口逻辑）
-        tradeWithHBService = new AlipayTradeWithHBServiceImpl.ClientBuilder().build();
-        monitorService = new AlipayMonitorServiceImpl.ClientBuilder().build();
-    }
-    public static void main(String[] args) {
-        AliPayUtil main = new AliPayUtil();
-
-        System.out.println("test alipay");
-        System.out.println("请输入条码：");
-        Scanner scanner = new Scanner(System.in);
-        String s = scanner.nextLine();
-        Boolean b = payLateFee(tradeService,s);
-        System.out.println();
-
-    }
-
-    // 简单打印应答
-    private void dumpResponse(AlipayResponse response) {
-        if (response != null) {
-            log.info(String.format("code:%s, msg:%s", response.getCode(), response.getMsg()));
-            if (!StringUtils.isEmpty(response.getSubCode())) {
-                log.info(String.format("subCode:%s, subMsg:%s", response.getSubCode(),
-                        response.getSubMsg()));
-            }
-            log.info("body:" + response.getBody());
-        }
-    }
-    // 缴纳滞纳金
-    public static Boolean payLateFee(AlipayTradeService service, String code){
-        String outTradeNo = "tradepay" + System.currentTimeMillis()
-                + (long) (Math.random() * 10000000L);
-        String subject = "缴纳滞纳金";
-        String totalAmount = "0.01";
-        String authCode = code;
-        String body = "滞纳金共计0.01元";
-        String operatorId = "1";
-        String storeId = "test_store_id";
-        String appAuthToken = "appid";
-        String timeoutExpress = "5m";
-        AlipayTradePayRequestBuilder builder = new AlipayTradePayRequestBuilder()
-                //            .setAppAuthToken(appAuthToken)
-                .setOutTradeNo(outTradeNo).setSubject(subject).setAuthCode(authCode)
-                .setTotalAmount(totalAmount).setStoreId(storeId)
-                .setBody(body).setOperatorId(operatorId)
-                .setSellerId("1").setTimeoutExpress(timeoutExpress);
-
-        // 调用tradePay方法获取当面付应答
-        AlipayF2FPayResult result = service.tradePay(builder);
-        switch (result.getTradeStatus()) {
-            case SUCCESS:
-                log.info("支付宝支付成功: )");
-                return true;
-
-            case FAILED:
-                log.error("支付宝支付失败!!!");
-                break;
-
-            case UNKNOWN:
-                log.error("系统异常，订单状态未知!!!");
-                break;
-
-            default:
-                log.error("不支持的交易状态，交易返回异常!!!");
-                break;
+        AlipayTradePayResponse response = null;
+        try {
+            response = alipayClient.execute(request);
+            System.out.println(response.getBody());
+            System.out.println(response.getTradeNo());
+        } catch (AlipayApiException e) {
+            e.printStackTrace();
         }
         return false;
-    }
-    // 测试当面付2.0支付
-    public void test_trade_pay(AlipayTradeService service) {
-        // (必填) 商户网站订单系统中唯一订单号，64个字符以内，只能包含字母、数字、下划线，
-        // 需保证商户系统端不能重复，建议通过数据库sequence生成，
-        String outTradeNo = "tradepay" + System.currentTimeMillis()
-                + (long) (Math.random() * 10000000L);
-
-        // (必填) 订单标题，粗略描述用户的支付目的。如“xxx品牌xxx门店消费”
-        String subject = "xxx品牌xxx门店当面付消费";
-
-        // (必填) 订单总金额，单位为元，不能超过1亿元
-        // 如果同时传入了【打折金额】,【不可打折金额】,【订单总金额】三者,则必须满足如下条件:【订单总金额】=【打折金额】+【不可打折金额】
-        String totalAmount = "0.01";
-
-        // (必填) 付款条码，用户支付宝钱包手机app点击“付款”产生的付款条码
-        String authCode = "用户自己的支付宝付款码"; // 条码示例，286648048691290423
-        // (可选，根据需要决定是否使用) 订单可打折金额，可以配合商家平台配置折扣活动，如果订单部分商品参与打折，可以将部分商品总价填写至此字段，默认全部商品可打折
-        // 如果该值未传入,但传入了【订单总金额】,【不可打折金额】 则该值默认为【订单总金额】- 【不可打折金额】
-        //        String discountableAmount = "1.00"; //
-
-        // (可选) 订单不可打折金额，可以配合商家平台配置折扣活动，如果酒水不参与打折，则将对应金额填写至此字段
-        // 如果该值未传入,但传入了【订单总金额】,【打折金额】,则该值默认为【订单总金额】-【打折金额】
-        String undiscountableAmount = "0.0";
-
-        // 卖家支付宝账号ID，用于支持一个签约账号下支持打款到不同的收款账号，(打款到sellerId对应的支付宝账号)
-        // 如果该字段为空，则默认为与支付宝签约的商户的PID，也就是appid对应的PID
-        String sellerId = "";
-
-        // 订单描述，可以对交易或商品进行一个详细地描述，比如填写"购买商品3件共20.00元"
-        String body = "购买商品3件共20.00元";
-
-        // 商户操作员编号，添加此参数可以为商户操作员做销售统计
-        String operatorId = "test_operator_id";
-
-        // (必填) 商户门店编号，通过门店号和商家后台可以配置精准到门店的折扣信息，详询支付宝技术支持
-        String storeId = "test_store_id";
-
-        // 业务扩展参数，目前可添加由支付宝分配的系统商编号(通过setSysServiceProviderId方法)，详情请咨询支付宝技术支持
-        String providerId = "2088100200300400500";
-        ExtendParams extendParams = new ExtendParams();
-        extendParams.setSysServiceProviderId(providerId);
-
-        // 支付超时，线下扫码交易定义为5分钟
-        String timeoutExpress = "5m";
-
-        // 商品明细列表，需填写购买商品详细信息，
-        List<GoodsDetail> goodsDetailList = new ArrayList<GoodsDetail>();
-        // 创建一个商品信息，参数含义分别为商品id（使用国标）、名称、单价（单位为分）、数量，如果需要添加商品类别，详见GoodsDetail
-        GoodsDetail goods1 = GoodsDetail.newInstance("goods_id001", "xxx面包", 1000, 1);
-        // 创建好一个商品后添加至商品明细列表
-        goodsDetailList.add(goods1);
-
-        // 继续创建并添加第一条商品信息，用户购买的产品为“黑人牙刷”，单价为5.00元，购买了两件
-        GoodsDetail goods2 = GoodsDetail.newInstance("goods_id002", "xxx牙刷", 500, 2);
-        goodsDetailList.add(goods2);
-
-        String appAuthToken = "应用授权令牌";//根据真实值填写
-
-        // 创建条码支付请求builder，设置请求参数
-        AlipayTradePayRequestBuilder builder = new AlipayTradePayRequestBuilder()
-                //            .setAppAuthToken(appAuthToken)
-                .setOutTradeNo(outTradeNo).setSubject(subject).setAuthCode(authCode)
-                .setTotalAmount(totalAmount).setStoreId(storeId)
-                .setUndiscountableAmount(undiscountableAmount).setBody(body).setOperatorId(operatorId)
-                .setExtendParams(extendParams).setSellerId(sellerId)
-                .setGoodsDetailList(goodsDetailList).setTimeoutExpress(timeoutExpress);
-
-        // 调用tradePay方法获取当面付应答
-        AlipayF2FPayResult result = service.tradePay(builder);
-        switch (result.getTradeStatus()) {
-            case SUCCESS:
-                log.info("支付宝支付成功: )");
-                break;
-
-            case FAILED:
-                log.error("支付宝支付失败!!!");
-                break;
-
-            case UNKNOWN:
-                log.error("系统异常，订单状态未知!!!");
-                break;
-
-            default:
-                log.error("不支持的交易状态，交易返回异常!!!");
-                break;
-        }
-    }
-
-    // 测试当面付2.0查询订单
-    public void test_trade_query() {
-        // (必填) 商户订单号，通过此商户订单号查询当面付的交易状态
-        String outTradeNo = "tradepay14817938139942440181";
-
-        // 创建查询请求builder，设置请求参数
-        AlipayTradeQueryRequestBuilder builder = new AlipayTradeQueryRequestBuilder()
-                .setOutTradeNo(outTradeNo);
-
-        AlipayF2FQueryResult result = tradeService.queryTradeResult(builder);
-        switch (result.getTradeStatus()) {
-            case SUCCESS:
-                log.info("查询返回该订单支付成功: )");
-
-                AlipayTradeQueryResponse response = result.getResponse();
-                dumpResponse(response);
-
-                log.info(response.getTradeStatus());
-                if (Utils.isListNotEmpty(response.getFundBillList())) {
-                    for (TradeFundBill bill : response.getFundBillList()) {
-                        log.info(bill.getFundChannel() + ":" + bill.getAmount());
-                    }
-                }
-                break;
-
-            case FAILED:
-                log.error("查询返回该订单支付失败或被关闭!!!");
-                break;
-
-            case UNKNOWN:
-                log.error("系统异常，订单支付状态未知!!!");
-                break;
-
-            default:
-                log.error("不支持的交易状态，交易返回异常!!!");
-                break;
-        }
-    }
-
-    // 测试当面付2.0退款
-    public void test_trade_refund() {
-        // (必填) 外部订单号，需要退款交易的商户外部订单号
-        String outTradeNo = "tradepay14817938139942440181";
-
-        // (必填) 退款金额，该金额必须小于等于订单的支付金额，单位为元
-        String refundAmount = "0.01";
-
-        // (可选，需要支持重复退货时必填) 商户退款请求号，相同支付宝交易号下的不同退款请求号对应同一笔交易的不同退款申请，
-        // 对于相同支付宝交易号下多笔相同商户退款请求号的退款交易，支付宝只会进行一次退款
-        String outRequestNo = "";
-
-        // (必填) 退款原因，可以说明用户退款原因，方便为商家后台提供统计
-        String refundReason = "正常退款，用户买多了";
-
-        // (必填) 商户门店编号，退款情况下可以为商家后台提供退款权限判定和统计等作用，详询支付宝技术支持
-        String storeId = "test_store_id";
-
-        // 创建退款请求builder，设置请求参数
-        AlipayTradeRefundRequestBuilder builder = new AlipayTradeRefundRequestBuilder()
-                .setOutTradeNo(outTradeNo).setRefundAmount(refundAmount).setRefundReason(refundReason)
-                .setOutRequestNo(outRequestNo).setStoreId(storeId);
-
-        AlipayF2FRefundResult result = tradeService.tradeRefund(builder);
-        switch (result.getTradeStatus()) {
-            case SUCCESS:
-                log.info("支付宝退款成功: )");
-                break;
-
-            case FAILED:
-                log.error("支付宝退款失败!!!");
-                break;
-
-            case UNKNOWN:
-                log.error("系统异常，订单退款状态未知!!!");
-                break;
-
-            default:
-                log.error("不支持的交易状态，交易返回异常!!!");
-                break;
-        }
     }
 }
