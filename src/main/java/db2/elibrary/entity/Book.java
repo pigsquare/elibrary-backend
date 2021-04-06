32cb819dc25da2fa4d6a9c36e49cd6481bf0c0bc
Li Guohan <2282224584@qq.com>
2021-04-06 20:21:03 +0800

    feat: book entity

--- src/main/java/db2/elibrary/entity/Book.java
+++ src/main/java/db2/elibrary/entity/Book.java
@@ -1,4 +1,28 @@
 package db2.elibrary.entity;
 
+import lombok.Data;
+import org.springframework.data.annotation.Reference;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class Book {
+    @Id
+    private String isbn;
+    private String name;
+    private String author;
+    private String publisher;
+    private String publishDate;
+    private Double price;
+    
+    @Column(length = 1024)
+    private String description;
+    private String keywords;
+    private String classifyCode;
+    private String indexNo;
+
+    private String adminId;
 }
