a979a4d7c17bc02ba022378d109e783c0d0ee2ca
zhufz <zhufz3528ui@126.com>
2021-04-13 22:38:06 +0800

    feat: add crawler to get book info 2

--- src/main/java/db2/elibrary/util/crawler/HttpUtilDownPage.java
+++ src/main/java/db2/elibrary/util/crawler/HttpUtilDownPage.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.htmlcleaner.HtmlCleaner;
 import org.htmlcleaner.TagNode;
 import org.htmlcleaner.XPatherException;
-import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -19,7 +18,7 @@ import java.net.MalformedURLException;
 public class HttpUtilDownPage {
     private static final WebClient webClient = new WebClient(BrowserVersion.CHROME);
 
-    public static String sendGet(String url){
+    private static String sendGet(String url){
         // js出错不抛出异常
         webClient.getOptions().setThrowExceptionOnScriptError(false);
         // HTTP！=200不抛出异常
@@ -52,11 +51,14 @@ public class HttpUtilDownPage {
             TagNode tagNode = htmlCleaner.clean(contents);
             String xpath1 = "/body/div[3]/h1/span";
             String xpath2 = "/body/div[3]/div[2]/div/div[1]/div[1]/div[1]/div[1]/div[2]/span[1]/a";
+            String xpath3 = "/body/div[3]/div[2]/div/div[1]/div[1]/div[1]/div[1]/div[2][text()[1]]";
+            String xpath4 = "/body/div[3]/div[2]/div/div[1]/div[3]/div[1]/div[1]/div/p";
+            String xpath5 = "/body/div[3]/div[2]/div/div[1]/div[3]/div[7]/div/span";
             var objArr = tagNode.evaluateXPath(xpath1);
             if(objArr!=null&&objArr.length>0){
                 for (Object obj: objArr){
                     TagNode tagNode1 = (TagNode)obj;
-                    String bookName = tagNode1.getText().toString();
+                    String bookName = removeWhiteLabels(tagNode1.getText().toString());
                     log.info("book name: "+bookName);
                 }
             }
@@ -64,12 +66,65 @@ public class HttpUtilDownPage {
             if(objArr!=null&&objArr.length>0){
                 for (Object obj: objArr){
                     TagNode tagNode1 = (TagNode)obj;
-                    String author = tagNode1.getText().toString();
+                    String author = removeWhiteLabels(tagNode1.getText().toString());
                     log.info("author: "+author);
                 }
             }
+            objArr = tagNode.evaluateXPath(xpath3);
+            if(objArr!=null&&objArr.length>0){
+                // TODO: 获取出版商信息、出版日期、定价
+                for (Object obj: objArr){
+                    TagNode tagNode1 = (TagNode)obj;
+                    String publisher = removeWhiteLabels(tagNode1.getText().toString());
+                    log.info("publisher: "+publisher);
+                }
+            }
+            objArr = tagNode.evaluateXPath(xpath4);
+            if(objArr!=null&&objArr.length>0){
+                for (Object obj: objArr){
+                    TagNode tagNode1 = (TagNode)obj;
+                    String bookDescription = removeWhiteLabels(tagNode1.getText().toString());
+                    log.info("book description: "+bookDescription);
+                }
+            }
+            objArr = tagNode.evaluateXPath(xpath5);
+            if(objArr!=null&&objArr.length>0){
+                for (Object obj: objArr){
+                    TagNode tagNode1 = (TagNode)obj;
+                    String keywords = removeWhiteLabels(tagNode1.getText().toString());
+                    log.info("keywords: "+keywords);
+                }
+            }
+
 
         }
 
+    }
+
+    private String removeWhiteLabels(String s){
+        int l = s.length();
+        int begin = 0;
+        int end = l-1;
+        for(int i=0;i<l;i++){
+            if(s.charAt(i)!='\n'&&s.charAt(i)!='\r'
+                    &&s.charAt(i)!='\t'&&s.charAt(i)!=' '){
+                begin=i;
+                break;
+            }
+        }
+        for(int i=l-1;i>=0;i--){
+            if(s.charAt(i)!='\n'&&s.charAt(i)!='\r'
+                    &&s.charAt(i)!='\t'&&s.charAt(i)!=' '){
+                end=i;
+                break;
+            }
+        }
+        if(begin>end)return "";
+        return s.substring(begin,end+1);
+    }
+
+    private String addInfo(String s, String info){
+        if(s.length()==0)return info;
+        return s+" "+info;
     }
 }
